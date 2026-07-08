4d0f338dc2a26fb0bbe7dfb8b5909ae62b0b3c82
Dmitriy <dionyaaa1554@gmail.com>
2026-07-08 17:37:53 +0300

    feat: добавил настройки пина PE14 для CS датчика BME280, исправил части кода, где могут возникнуть ошибки в будущем

--- include/board.h
+++ include/board.h
@@ -6,6 +6,8 @@
 extern UART_HandleTypeDef huart3;
 extern I2C_HandleTypeDef hi2c1;
 
+void Error_Handler(void);
+
 void SystemClock_Config(void);
 void MX_GPIO_Init(void);
 void MX_USART3_UART_Init(void);
--- src/board.c
+++ src/board.c
@@ -66,9 +66,7 @@ void MX_GPIO_Init(void)
     // Включение тактирования портов:
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
-
-    // Выключение пинов PB0, PB7 и PB14 (светодиоды):
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_14 | GPIO_PIN_7, GPIO_PIN_RESET);
+    __HAL_RCC_GPIOE_CLK_ENABLE();
 
     // Настройка пинов PB0, PB7 и PB14 (светодиоды):
     GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_14 | GPIO_PIN_7;
@@ -82,6 +80,19 @@ void MX_GPIO_Init(void)
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_PULLDOWN;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    // Настройка пина PE14 (CS для BME280):
+    GPIO_InitStruct.Pin = GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    // Выключение пинов PB0, PB7 и PB14 (светодиоды):
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_14 | GPIO_PIN_7, GPIO_PIN_RESET);
+
+    // Включение пина PE14 (CS для BME280):
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_14, GPIO_PIN_SET);
 }
 
 // Функции инициализации USART3:
@@ -148,9 +159,9 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
         __HAL_RCC_I2C1_CLK_ENABLE();
         __HAL_RCC_GPIOB_CLK_ENABLE();
 
-        GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_9;
+        GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_9;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Pull = GPIO_PULLUP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
