10c4f89c5ecaf8d3fb3db654a7ad1b042e4da31b
Dmitriy <dionyaaa1554@gmail.com>
2026-07-11 18:51:02 +0300

    perf: включил работу PLL для увеличения частоты HSI, настроил SYSCLK, HCLK и PCLK2 на 80 МГц и PCLK1 на 40 МГц

--- src/board.c
+++ src/board.c
@@ -29,7 +29,7 @@ void SystemClock_Config(void)
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    // Настройка осциллятора (пока без PLL, 16 МГц):
+    // Настройка осциллятора:
     __HAL_RCC_PWR_CLK_ENABLE();
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
@@ -37,22 +37,29 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
 
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    // Настройки PLL:
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 80;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+    RCC_OscInitStruct.PLL.PLLR = 2;
 
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
         Error_Handler();
     }
 
-    // Настройка шин (пока все по 16 МГц):
+    // Настройка шин:
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; // 80 МГц
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; // 80 МГц
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2; // 40 МГц
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1; // 80 МГц
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
     {
         Error_Handler();
     }
